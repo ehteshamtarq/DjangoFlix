af9983b1beaa201b22d88a09f5ea5d85153c8f8e
EHTESHAM TARIQUE <tariqueehtesham1@gmail.com>
2024-11-19 14:43:49 +0530

    video app and model initialization

---
+++ videos/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+class Video(models.Model):
+    title = models.CharField(max_length= 220)
+    description =  models.TextField(blank = True, null = True)
+    slug = models.SlugField(blank = True, null = True)
+    video_id = models.CharField(max_length= 220)
+    # timestamp
+    # state
+
