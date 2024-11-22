d1f60e8d0645ac2308ff8de08890c2896018a158
EHTESHAM TARIQUE <tariqueehtesham1@gmail.com>
2024-11-22 16:02:52 +0530

    adding attributes to our models and writing customs manager and signals

---
+++ djangoflix/db/models.py
@@ -0,0 +1,5 @@
+from django.db import models
+
+class PublishStateOptions(models.TextChoices):
+    PUBLISH = 'PU', 'Publish'
+    DRAFT = 'DR', 'Draft'
--- videos/admin.py
+++ videos/admin.py
@@ -7,7 +7,7 @@ class VideoAllAdmin(admin.ModelAdmin):
     list_display = ['title', 'id','video_id', 'is_published']
     search_fields = ['title']
     list_filter = ['active']
-    readonly_fields = ['id', 'is_published']
+    readonly_fields = ['id', 'is_published', 'publish_timestamp']
     class Meta:
         model = VideoAllProxy
 
